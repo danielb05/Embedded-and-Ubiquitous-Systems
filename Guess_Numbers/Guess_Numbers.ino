1138e69efc3513528668367f48ade8b985070fda
Daniel <dan.b.v.r05@gmail.com>
2019-10-29 19:31:32 +0100

    Game ready

--- Guess_Numbers/Guess_Numbers.ino
+++ Guess_Numbers/Guess_Numbers.ino
@@ -2,19 +2,35 @@ const byte numChars = 32;
 char receivedChars[numChars]; // an array to store the received data
 
 int number;
-int tip;
+int attempts;
+int numberClues;
+
+bool gameOngoing = false;
 
 void setup()
 {
+  randomSeed(analogRead(0));
   Serial.begin(9600);
-  Serial.println("Guess a number from 1 to 1000");
-  number = random(1000);
-  Serial.println(number);
 }
 
 void loop()
 {
+  if (!gameOngoing)
+  {
+    setUpGame();
+  }
   recvWithEndMarker();
+}
+
+void setUpGame()
+{
+  Serial.println("Guess a number from 1 to 1000");
+  Serial.println("\nPress h for a clue. \n");
+  number = random(1001);
+  //Serial.println(number);
+  gameOngoing = true;
+  attempts = 0;
+  numberClues = 0;
 }
 
 void recvWithEndMarker()
@@ -40,20 +56,29 @@ void recvWithEndMarker()
     {
       receivedChars[ndx] = '\0'; // terminate the string
       ndx = 0;
-      showNewData();
+      avaluateInput();
     }
   }
 }
 
-void showNewData()
+void avaluateInput()
 {
   if (receivedChars[0] >= 48 && receivedChars[0] <= 57)
   {
+    attempts++;
     int input = -1;
     input = atoi(receivedChars);
     if (input == number)
     {
-      Serial.println("You are correct!");
+      Serial.println("\nYou are correct!");
+      Serial.print("\nThe number was: ");
+      Serial.println(number);
+      Serial.print("\nNumber of attempts needed: ");
+      Serial.println(attempts);
+      Serial.print("Number of clues needed: ");
+      Serial.println(numberClues);
+      Serial.println("\n\nYOU WON!!!\n\n");
+      gameOngoing = false;
     }
     else if (input > number)
     {
@@ -70,6 +95,65 @@ void showNewData()
   }
 }
 
-void randomHelp(){
-  Serial.println("Help");
+void randomHelp()
+{
+
+  numberClues++;
+  int help = random(4);
+  //Serial.println(help);
+  switch (help)
+  {
+
+  case 0: // Even or Odd
+    if (number % 2 == 0)
+    {
+      Serial.println("It is an EVEN number.");
+    }
+    else
+    {
+      Serial.println("It is an ODD number.");
+    }
+    break;
+
+  // number of digits
+  case 1:
+    if (number > 999)
+    {
+      Serial.println("The number has 4 digits - xxxx");
+    }
+    else if (number > 99 && number < 999)
+    {
+      Serial.println("The number has 3 digits - xxx");
+    }
+    else if (number < 99 && number > 9)
+    {
+      Serial.println("The number has 2 digits - xx");
+    }
+    else
+    {
+      Serial.println("The number has 1 digit - x");
+    }
+    break;
+
+  // Last digit
+  case 2:
+
+    Serial.print("The last digit of the number is: ");
+    Serial.println(number % 10);
+    break;
+
+    // First digit
+  case 3:
+
+    int num = number;
+    while (num >= 10)
+    {
+      num = num / 10;
+    }
+
+    Serial.print("The first digit of the number is: ");
+    Serial.println(num);
+    break;
+    
+  }
 }
